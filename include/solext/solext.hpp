705bcc7e42c0c0c8a788c619e45dcc6e92a212c0
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-01-03 00:31:35 +0100

    added add_module

--- include/solext/solext.hpp
+++ include/solext/solext.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include <sol/sol.hpp>
 
 namespace solext {
@@ -9,12 +10,17 @@ namespace internal {
 class StatePrivate;
 }
 
-class State : public sol::state {
+class State : public sol::state, std::enable_shared_from_this<State> {
 
 public:
   State();
   State(int argc, const char **args);
   ~State();
+
+  template <typename T, typename... Args> void add_module(Args... args) {
+    std::string name = T::module_name();
+    add_module(name, new T(this, std::forward_list<Args>(args)...));
+  }
 
   template <typename T> void add_module() {
     std::string name = T::module_name();

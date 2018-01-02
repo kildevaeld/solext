a252fb7422bddfdbd7f7c23a635471ded44a2aae
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-01-03 00:11:39 +0100

    updated csystem

--- example/main.cpp
+++ example/main.cpp
@@ -2,7 +2,6 @@
 
 int main(int argc, const char **argv) {
   solext::State state(argc, argv);
-
   state.open_libraries(sol::lib::base);
 
   state.script("print(process.homedir()); print(process.cwd())");
